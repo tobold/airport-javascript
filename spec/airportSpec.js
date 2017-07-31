e6862ba806786bd15d5991d7f8d5a9879f66c1f6
Toby Hamand <tobyhamand@gmail.com>
2017-07-31 15:44:18 +0100

    setup first test

---
+++ spec/airportSpec.js
@@ -0,0 +1,15 @@
+describe("Airport", function() {
+  var airport;
+  var plane;
+
+  beforeEach(function() {
+    airport = new Airport();
+  });
+
+  describe("landing a plane", function() {
+    it("lands a plane", function() {
+      airport.land(plane);
+      expect(airport.planes).toInclude(plane);
+    });
+  });
+});
